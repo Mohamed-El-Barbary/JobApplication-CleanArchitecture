93ad351010d63919bf7ad549885e86dc30be2c8e
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2026-09-20 02:42:17 +0300

    feat(Infrastructure) :- Implement UnitOfWork and Generic Repository

---
+++ JobApplication.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using JobApplication.Domain.Entities.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobApplication.Domain.Repositories;
+
+public interface IUnitOfWork
+{
+
+    IRepository<T> Repository<T>() where T : BaseEntity;
+
+    Task<int> SaveChangesAsync(CancellationToken ck = default);
+
+}
